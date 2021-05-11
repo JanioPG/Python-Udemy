c7ab1cf00f95efef872f274c38d42297ed7e38be
Jânio Prado Garcia <81107633+JanioPG@users.noreply.github.com>
2021-05-11 14:56:23 -0300

    Update homework.py

--- 10-Errors_and_Exceptions_Handling/homework.py
+++ 10-Errors_and_Exceptions_Handling/homework.py
@@ -1,19 +1,21 @@
 # Problema 1
-try: 
-    for i in ['a', 'b', 'c']:
-        print(i ** 2)
-except:
-    print('Error: Operação não suporta a potência de uma string')
+def problem1():
+    try: 
+        for i in ['a', 'b', 'c']:
+            print(i ** 2)
+    except:
+        print('Error: Operação não suporta a potência de uma string')
 
 # Problema 2
-try:
-    x = 5
-    y = 0
-    z = x / y
-except ZeroDivisionError:
-    print('Error: A divisão por zero é uma indefinição matemática.')
-finally:
-    print('All Done')
+def problem2():
+    try:
+        x = 5
+        y = 0
+        z = x / y
+    except ZeroDivisionError:
+        print('Error: A divisão por zero é uma indefinição matemática.')
+    finally:
+        print('All Done')
 
 # Problema 3
 def ask():
@@ -27,4 +29,7 @@ def ask():
     valor = number ** 2
     print(f'Thank you, your number squared is: {valor}')
     
-ask()
+if __name__ == '__main__':
+    problem1()
+    problem2()
+    ask()
